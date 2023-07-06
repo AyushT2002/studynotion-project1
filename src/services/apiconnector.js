0c777a2779117ade067704bf46c367283cf558a2
AyushT2002 <ayushbthakare3@gmail.com>
2023-07-06 21:38:35 +0530

    first commit

---
+++ src/services/apiconnector.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+export const axiosInstance = axios.create({});
+
+export const apiConnector = (method, url, bodyData, headers, params) => {
+        console.log("////////////////////////////////////////////////////");
+        console.log(url);
+        console.log(method);
+        console.log(bodyData);
+    return axiosInstance({
+        method:`${method}`,
+        url:`${url}`,
+        data: bodyData ? bodyData : null,
+        headers: headers ? headers: null,
+        params: params ? params : null,
+    });
+}
